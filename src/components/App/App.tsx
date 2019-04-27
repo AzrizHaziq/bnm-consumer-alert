cafdc00da4694936902e4f18cbcbc49f051d39f8
AzrizHaziq <azrizhaziq@gmail.com>
2019-04-27 09:11:51 +0800

    working on get bnm api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 const App: React.FC = () => {
+  const [alertList, setAlertList] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState({})
+
+  async function getConsumerAlert() {
+    setLoading(true)
+
+    try {
+      const url = 'https://api.bnm.gov.my/public/consumer-alert'
+
+      const response = await fetch(url, {
+        headers: {
+          Accept: 'application/vnd.BNM.API.v1+json',
+        },
+      })
+
+      const json = await response.json()
+
+      setAlertList(json)
+    } catch (e) {
+      setError({ e, msg: 'Error getting consumer alert' })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    const res = getConsumerAlert().then(console.log)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
