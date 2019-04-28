5e158390f4d78f14f891119a3ffeac3d2a9c4376
AzrizHaziq <azrizhaziq@gmail.com>
2019-04-28 19:10:02 +0800

    add custom scrollbar ui

--- src/components/Consumer/Consumers.tsx
+++ src/components/Consumer/Consumers.tsx
@@ -6,7 +6,7 @@ import Consumer from './Consumer'
 
 const Consumers: React.FC<{ item: ConsumerAlert[] }> = ({ item }) => {
   return (
-    <div className="consumers__list">
+    <div className="consumers__list" id="custom-scrollbar">
       <ul className="pl-0 w-100">
         {item.map((data: ConsumerAlert) => (
           <Consumer key={data.id} item={data} />
