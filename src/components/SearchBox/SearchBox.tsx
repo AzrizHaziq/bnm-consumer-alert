1be1ec389793052281745cc05603f35b6d916800
AzrizHaziq <azrizhaziq@gmail.com>
2019-04-29 20:24:31 +0800

    done filtering

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import { escapeRegExp } from 'helpers'
+import React, { useContext, useState } from 'react'
+import {
+  ConsumerAlertContext,
+  IConsumerContext,
+} from 'data/consumer-alert.context'
+
 import './SearchBox.scss'
 
-const SearchBox: React.FC = () => (
-  <div className="row justify-content-center">
-    <div className="col-sm-12 col-lg-10">
-      <form>
+const SearchBox: React.FC = () => {
+  const [value, setValue] = useState('')
+
+  const { consumerAlerts, setConsumerAlerts, resetConsumerAlerts } = useContext<
+    IConsumerContext
+  >(ConsumerAlertContext)
+
+  const onChange = (e: any) => {
+    const { value: val } = e.target
+    setValue(val)
+
+    if (val === '') {
+      resetConsumerAlerts()
+      return
+    }
+
+    const filterConsumerAlerts = consumerAlerts.filter(consumer => {
+      const reg = new RegExp(escapeRegExp(val), 'ig')
+      const { name } = consumer
+
+      return reg.test(name)
+    })
+
+    setConsumerAlerts(filterConsumerAlerts)
+  }
+
+  return (
+    <div className="row justify-content-center">
+      <div className="col-sm-12 col-lg-10">
         <input
+          type="text"
           name="search-box"
           className="form__search"
           placeholder="Abu & Ali Sdn Bhd"
+          value={value}
+          onChange={onChange}
         />
-      </form>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default SearchBox
--- src/data/consumer-alert.context.tsx
+++ src/data/consumer-alert.context.tsx
@@ -1,21 +1,29 @@
 import React, { createContext, useState } from 'react'
 import { consumerAlert, IConsumerAlert } from 'data/consumer-alert'
 
-const ConsumerAlertContext = createContext({})
+const ConsumerAlertContext = createContext<IConsumerContext>({
+  consumerAlerts: [],
+  setConsumerAlerts: () => {},
+  resetConsumerAlerts: () => {},
+})
 
 const ConsumerAlertProvider = ConsumerAlertContext.Provider
-const ConsumerAlertConsumer = ConsumerAlertContext.Consumer
 
 function ConsumerAlert({ children }: any) {
   const [consumerAlerts, setConsumerAlerts] = useState<IConsumerAlert[]>(
     consumerAlert,
   )
 
+  function resetConsumerAlerts() {
+    setConsumerAlerts(consumerAlert)
+  }
+
   return (
     <ConsumerAlertProvider
       value={{
         consumerAlerts,
         setConsumerAlerts,
+        resetConsumerAlerts,
       }}
     >
       {children}
@@ -23,9 +31,10 @@ function ConsumerAlert({ children }: any) {
   )
 }
 
-export {
-  ConsumerAlert,
-  ConsumerAlertContext,
-  ConsumerAlertProvider,
-  ConsumerAlertConsumer,
+export { ConsumerAlert, ConsumerAlertContext, ConsumerAlertProvider }
+
+export interface IConsumerContext {
+  consumerAlerts: IConsumerAlert[]
+  setConsumerAlerts: any
+  resetConsumerAlerts: any
 }
