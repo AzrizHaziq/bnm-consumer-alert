96b53ad543b6b03b82d98bffb572977b238cb624
AzrizHaziq <azrizhaziq@gmail.com>
2019-04-29 21:45:21 +0800

    added clock

--- src/components/Consumer/Consumer.tsx
+++ src/components/Consumer/Consumer.tsx
@@ -34,21 +34,29 @@ const Link: React.FC<{ url: string }> = ({ url }) => {
 }
 
 const Consumer: React.FC<{ item: IConsumerAlert }> = ({ item }) => {
-  const { name, registration_number, websites } = item
+  const { name, registration_number, websites, added_date } = item
   const avatar = name.slice(0, 2).toUpperCase()
 
   return (
     <li className="consumer__list consumer__list-style">
       <div className={`consumer__avatar ${randomBgCssColor()}`}>{avatar}</div>
-      <div className="consumer-content">
-        <p className="mb-0">
-          {name}
-          {registration_number && (
-            <span className="consumer__registration-number ml-1 mb-0">
-              ({registration_number})
-            </span>
-          )}
-        </p>
+      <div className="consumer__content">
+        <div className="consumer__content__heading">
+          <p className="mb-0">
+            {name}
+            {registration_number && (
+              <span className="consumer__registration-number ml-1 mb-0">
+                ({registration_number})
+              </span>
+            )}
+          </p>
+          <div className="consumer__date-added">
+            <svg viewBox="0 0 24 24">
+              <path d="M12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22C6.47,22 2,17.5 2,12A10,10 0 0,1 12,2M12.5,7V12.25L17,14.92L16.25,16.15L11,13V7H12.5Z" />
+            </svg>
+            <small className="consumer--muted">{added_date}</small>
+          </div>
+        </div>
         <ul className="consumer__list__websites consumer__list-style">
           {websites.map(website => {
             const url: any = website.trim()
