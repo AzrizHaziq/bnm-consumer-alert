dbe54e0bd81950b804ea6bd687037bc3cb353a8a
AzrizHaziq <azrizhaziq@gmail.com>
2019-05-20 09:55:31 +0800

    update layout for sort btns

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -15,19 +15,23 @@ const ArrowIcon: React.FC<{ sort: 'asc' | 'desc' }> = ({ sort }) =>
   )
 
 const Sort: React.FC = () => (
-  <div className="sort">
-    <div className="sort-alphabet mr-1">
-      <ArrowIcon sort="asc" />
-      <svg className="sort__icon" viewBox="0 0 24 24">
-        <path d="M6,11A2,2 0 0,1 8,13V17H4A2,2 0 0,1 2,15V13A2,2 0 0,1 4,11H6M4,13V15H6V13H4M20,13V15H22V17H20A2,2 0 0,1 18,15V13A2,2 0 0,1 20,11H22V13H20M12,7V11H14A2,2 0 0,1 16,13V15A2,2 0 0,1 14,17H12A2,2 0 0,1 10,15V7H12M12,15H14V13H12V15Z" />
-      </svg>
-    </div>
+  <div className="row justify-content-center">
+    <div className="col-sm-12 col-lg-10">
+      <div className="sort">
+        <div className="sort-alphabet mr-1">
+          <ArrowIcon sort="asc" />
+          <svg className="sort__icon" viewBox="0 0 24 24">
+            <path d="M6,11A2,2 0 0,1 8,13V17H4A2,2 0 0,1 2,15V13A2,2 0 0,1 4,11H6M4,13V15H6V13H4M20,13V15H22V17H20A2,2 0 0,1 18,15V13A2,2 0 0,1 20,11H22V13H20M12,7V11H14A2,2 0 0,1 16,13V15A2,2 0 0,1 14,17H12A2,2 0 0,1 10,15V7H12M12,15H14V13H12V15Z" />
+          </svg>
+        </div>
 
-    <div className="sort-date">
-      <ArrowIcon sort="desc" />
-      <svg className="sort__icon" viewBox="0 0 24 24">
-        <path d="M12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22C6.47,22 2,17.5 2,12A10,10 0 0,1 12,2M12.5,7V12.25L17,14.92L16.25,16.15L11,13V7H12.5Z" />
-      </svg>
+        <div className="sort-date">
+          <ArrowIcon sort="desc" />
+          <svg className="sort__icon" viewBox="0 0 24 24">
+            <path d="M12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22C6.47,22 2,17.5 2,12A10,10 0 0,1 12,2M12.5,7V12.25L17,14.92L16.25,16.15L11,13V7H12.5Z" />
+          </svg>
+        </div>
+      </div>
     </div>
   </div>
 )
