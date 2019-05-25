1697b3a6b6cdeb2c66f8a1e4629486e14b8e2ef2
AzrizHaziq <azrizhaziq@gmail.com>
2019-05-25 16:33:05 +0800

    move github fork btn to index.html as it doenst really need js to render it
    - delete github fork component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,6 @@ import Consumers from 'components/Consumer/Consumers'
 import SearchBox from 'components/SearchBox/SearchBox'
 import EmptyState from 'components/EmptyState/EmptyState'
 import { ConsumerAlert } from 'data/consumer-alert.context'
-import { GithubFork } from 'components/GithubFork/GithubFork'
 import ErrorMessage from 'components/ErrorMessage/ErrorMesssage'
 
 import './App.scss'
@@ -49,46 +48,43 @@ const App: React.FC = () => {
   }, [])
 
   return (
-    <>
-      <GithubFork />
-      <div className="main">
-        {loading ? (
-          <div className="flex-all-center">
-            <Spinner />
-          </div>
-        ) : error ? (
-          <div className="flex-all-center">
-            <ErrorMessage str={error.msg} />
-          </div>
-        ) : consumerList === null ? null : consumerList.length <= 0 ? (
-          <div className="flex-all-center">
-            <EmptyState str="Oops might need to add list here" />
-          </div>
-        ) : (
-          <div className="consumer-alert__main">
-            <ConsumerAlert consumerList={consumerList}>
-              <div className="container mt-5">
-                <div className="row justify-content-center">
-                  <div className="col-sm-12 col-lg-10">
-                    <h2 className="text-white-50 mb-4 mr-4">
-                      BNM Consumer Alert
-                    </h2>
-                  </div>
+    <div className="main">
+      {loading ? (
+        <div className="flex-all-center">
+          <Spinner />
+        </div>
+      ) : error ? (
+        <div className="flex-all-center">
+          <ErrorMessage str={error.msg} />
+        </div>
+      ) : consumerList === null ? null : consumerList.length <= 0 ? (
+        <div className="flex-all-center">
+          <EmptyState str="Oops might need to add list here" />
+        </div>
+      ) : (
+        <div className="consumer-alert__main">
+          <ConsumerAlert consumerList={consumerList}>
+            <div className="container mt-5">
+              <div className="row justify-content-center">
+                <div className="col-sm-12 col-lg-10">
+                  <h2 className="text-white-50 mb-4 mr-4">
+                    BNM Consumer Alert
+                  </h2>
                 </div>
-                <SearchBox />
-                <div className="mb-2" />
-                <Sort />
-                <div className="mb-2" />
-                <Consumers />
-                <div className="mb-5" />
-                <Tags consumerList={consumerList} />
-                <div className="mb-5" />
               </div>
-            </ConsumerAlert>
-          </div>
-        )}
-      </div>
-    </>
+              <SearchBox />
+              <div className="mb-2" />
+              <Sort />
+              <div className="mb-2" />
+              <Consumers />
+              <div className="mb-5" />
+              <Tags consumerList={consumerList} />
+              <div className="mb-5" />
+            </div>
+          </ConsumerAlert>
+        </div>
+      )}
+    </div>
   )
 }
 
